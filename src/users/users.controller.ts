215b55a373666b50f13f403baa226ecc15ddde57
v0m04nb <vignesh.mohan0@walmart.com>
2023-04-23 14:01:39 +0530

    Used pipe and DTO to validate the request

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDTO } from './dtos/create-user.dto';
+import { log } from 'console';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+  @Post('/signUp')
+  createUser(@Body() body: CreateUserDTO) {
+    log(body);
+  }
+}
