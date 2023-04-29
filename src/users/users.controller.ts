54fa72ab0019cbca16d269abdb9d982f3029b753
v0m04nb <vignesh.mohan0@walmart.com>
2023-04-29 22:29:43 +0530

    CRUD Operations in Controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { CreateUserDTO } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
+import { UpdateUserDTO } from './dtos/update-user.dto';
 
 @Controller('auth')
 export class UsersController {
@@ -8,5 +18,24 @@ export class UsersController {
   @Post('/signUp')
   createUser(@Body() body: CreateUserDTO) {
     this.userService.create(body.email, body.password);
+  }
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    return this.userService.findOne(parseInt(id));
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.userService.find(email);
+  }
+
+  @Delete('/:id')
+  deleteUser(@Param('id') id: string) {
+    return this.userService.remove(parseInt(id));
+  }
+
+  @Patch('/:id')
+  updateUserData(@Param('id') id: string, @Body() body: UpdateUserDTO) {
+    return this.userService.update(parseInt(id), body);
   }
 }
