d32e8a20e7c8dc181f19573b8a8b82aa04c4a645
Vignesh Mohan <vigneshmohan@Vigneshs-MacBook-Air.local>
2023-04-30 23:10:59 +0530

    Controller level interceptors

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -14,6 +14,7 @@ import { UpdateUserDTO } from './dtos/update-user.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 
+@Serialize(UserDto)
 @Controller('auth')
 export class UsersController {
   constructor(private userService: UsersService) {}
@@ -23,7 +24,7 @@ export class UsersController {
   }
 
   // @UseInterceptors(new SerializeInterceptor(UserDto))
-  @Serialize(UserDto)
+  // @Serialize(UserDto)
   @Get('/:id')
   findUser(@Param('id') id: string) {
     return this.userService.findOne(parseInt(id));
