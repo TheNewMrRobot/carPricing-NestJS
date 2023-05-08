05ec0936104b5bf4fdf98135b24660f822fee4dd
Vignesh Mohan <vigneshmohan@Vigneshs-MacBook-Air.local>
2023-05-08 07:15:01 +0530

    Create Report in controller and services

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { CreateReportDTO } from './dtos/CreateReport.dto';
+import { ReportsService } from './reports.service';
+import { AuthGaurd } from 'src/guards/auth.guard';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+  constructor(private reportService: ReportsService) {}
+  @Post()
+  @UseGuards(AuthGaurd)
+  createReport(@Body() body: CreateReportDTO) {
+    this.reportService.create(body);
+  }
+}
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Report } from './reports.entity';
+import { CreateReportDTO } from './dtos/CreateReport.dto';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(@InjectRepository(Report) public repo: Repository<Report>) {}
+  create(reportDto: CreateReportDTO) {
+    const report = this.repo.create(reportDto);
+    return this.repo.save(report);
+  }
+}
